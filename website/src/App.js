9d993d980a536a7f28a449af8ae19fd18209f81c
Sebbyjones <sebastianlatempa@gmail.com>
2026-01-17 15:59:18 -0700

    adding react

---
+++ website/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Sebastian Latempa</h1>
+        <p>Welcome to my personal portfolio</p>
+        <p>More content coming soon!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
