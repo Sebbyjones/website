ccf1e13b84a09d77afbb65abdc6209403d59a296
Sebbyjones <sebastianlatempa@gmail.com>
2026-01-18 10:25:03 -0700

    testingpicture

--- website/src/components/footer/Footer.js
+++ website/src/components/footer/Footer.js
@@ -1,6 +1,7 @@
 import React, {useContext} from "react";
 import "./Footer.scss";
 import {Fade} from "react-reveal";
+import emoji from "react-easy-emoji";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
@@ -8,7 +9,18 @@ export default function Footer() {
   return (
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
-        {/* Removed all text content for an empty footer */}
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          {emoji("")}
+        </p>
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          Theme by{" "}
+          <a
+            href=""
+            target="_blank"
+            rel="noreferrer"
+          >
+          </a>
+        </p>
       </div>
     </Fade>
   );
