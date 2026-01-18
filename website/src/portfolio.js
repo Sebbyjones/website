5d4e0e92c322baf90f5367d0b4246e6073fac150
Sebbyjones <sebastianlatempa@gmail.com>
2026-01-18 09:55:14 -0700

    experience

--- website/src/portfolio.js
+++ website/src/portfolio.js
@@ -190,7 +190,7 @@ const workExperiences = {
     {
       role: "Lead Software Engineer",
       company: "State Farm",
-      companylogo: require("./assets/images/statefarmlogo.jpg"),
+      companylogo: require("./assets/images/statefarmlogo.png"),
       date: "2016 – Present",
       desc: "Architected and maintained enterprise-wide event-driven API platform supporting 45+ business units, processing 4.8B+ customer interactions yearly.",
       descBullets: [
