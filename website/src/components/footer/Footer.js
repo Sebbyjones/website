1f0a3f8022e3c9dcc067be689ea3a7c3eac5febe
Sebbyjones <sebastianlatempa@gmail.com>
2026-01-18 10:29:56 -0700

    testingpicture

--- website/src/components/footer/Footer.js
+++ website/src/components/footer/Footer.js
@@ -15,7 +15,7 @@ export default function Footer() {
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
           <a
-            href=""
+            href="www.github.com/sebbyjones"
             target="_blank"
             rel="noreferrer"
           >
